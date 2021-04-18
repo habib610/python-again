46e27c3f34aa4887b8a3cbf14b7429151a2ddfad
habib610 <habib54562@gmail.com>
2021-04-18 15:00:55 +0600

    module

---
+++ Fibonacci/program.py
@@ -0,0 +1,8 @@
+import trial
+
+print("Hello, I am inside programm.py")
+print(trial.__name__)
+print("What is the name of porgram.py now:", __name__)
+# n = fibo.find_fib(5)
+#
+# print("5th fibonacci is", n)
---
+++ Fibonacci/trial.py
@@ -0,0 +1,4 @@
+print("i am trail module")
+
+if __name__ == "__main__":
+    print("I am main module of trial: Okay!")
