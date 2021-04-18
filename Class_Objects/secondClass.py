c3f31aed4be1e2911bd38d7ae95a964503828600
habib610 <habib54562@gmail.com>
2021-04-18 20:00:26 +0600

    multiple-class

--- Class_Objects/secondClass.py
+++ Class_Objects/secondClass.py
@@ -4,11 +4,20 @@ class Car:
         self.color = c
 
     def start(self):
-        print("Starting the class", self.name)
+        print("Starting the class", '"', self.name,'"')
 
 
 my_car = Car("Lamborghini", "Red")
-
 print(my_car.name)
 print(my_car.color)
 my_car.start()
+
+my_car1 = Car("Marcidies", "Black")
+print(my_car1.name)
+print(my_car1.color)
+my_car1.start()
+
+my_car2 = Car("Corolla", "White")
+print(my_car2.name)
+print(my_car2.color)
+my_car2.start()
