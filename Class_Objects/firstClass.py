67720bb54bb864c589eaa49c08b7035c3c3d52e7
habib610 <habib54562@gmail.com>
2021-04-18 16:05:01 +0600

    accesing-class-atribute-in-method

--- Class_Objects/firstClass.py
+++ Class_Objects/firstClass.py
@@ -2,13 +2,15 @@ class Car:
     name = ""  # this are called daa attribute
     color = ""  # this are called daa attribute
 
+    # deffining the objects attribute at here
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
     # when we write function in a class or object its called method
-    def start(self):  # This one is called method
-        print("starting car", Car.name)
+    def start(self, name):  # This one is called method
+        print("starting car", name)
 
-
-Car.name = "Toyota"
-Car.color = "Blue"
 
 print(Car.name)
 print(Car.color)
@@ -16,10 +18,10 @@ print(Car.color)
 
 # Here we will  create objects or instances of Class Car
 print("Car class objects or instances are here>>>>>>>>>>>>>")
-my_car = Car()
+my_car = Car("Marcidies", "Red")
 my_car.name = "Marcidies"
 my_car.color = "Brown"
 
 print(my_car.name)
 print(my_car.color)
-my_car.start()
+my_car.start("volvo")
---
+++ Class_Objects/secondClass.py
@@ -0,0 +1,14 @@
+class Car:
+    def __init__(self, n, c):
+        self.name = n
+        self.color = c
+
+    def start(self):
+        print("Starting the class", self.name)
+
+
+my_car = Car("Lamborghini", "Red")
+
+print(my_car.name)
+print(my_car.color)
+my_car.start()
